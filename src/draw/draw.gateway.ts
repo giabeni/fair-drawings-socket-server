b8d6191364389892e57fefecad0876bca52cd5a9
Giovanni Abeni <giovanni.abeni.santos@usp.br>
2020-12-04 01:53:13 -0300

    Improved draw event handling

--- src/draw/draw.gateway.ts
+++ src/draw/draw.gateway.ts
@@ -1,4 +1,4 @@
-import { OnModuleInit, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import {
   WebSocketGateway,
   WebSocketServer,
@@ -16,6 +16,11 @@ import { FirebaseAuthGuard } from '../guards/firebase-auth.guard';
 import { FirebaseAdmin, InjectFirebaseAdmin } from 'nestjs-firebase';
 import { CollectionReference, QuerySnapshot } from '@google-cloud/firestore';
 import { firestore, auth } from 'firebase-admin';
+import { Draw } from './entities/draw.entity';
+import { DrawEventType } from './enums/draw-event-type.enum';
+import { Candidate } from './entities/candidate.entity';
+import { Stakeholder } from './entities/stakeholder.entity';
+import { DrawEvent } from './interfaces/draw-event.interface';
 
 @WebSocketGateway()
 export class DrawGateway
@@ -23,7 +28,7 @@ export class DrawGateway
   @WebSocketServer()
   server: Server;
 
-  draws: any[] = [];
+  draws: Draw[] = [];
 
   drawsRef: CollectionReference<any>;
 
@@ -68,51 +73,69 @@ export class DrawGateway
   @UseGuards(FirebaseAuthGuard)
   @SubscribeMessage('createDraw')
   async onCreateDraw(
-    @MessageBody() data: any,
+    @MessageBody() drawBody: Draw,
     @ConnectedSocket() client: Socket,
   ) {
     console.log(
-      `🚀 ~ file: draw.gateway.ts ~ line 52 ~ DrawGateway ~ data`,
-      data,
+      `🚀 ~ file: draw.gateway.ts ~ line 52 ~ DrawGateway ~ drawBody`,
+      drawBody,
     );
 
-    const createdDoc = await this.drawsRef.add(data).catch((err) => {
-      console.error(err);
-      throw new WsException('ERR_CREATE_DRAW');
-    });
+    drawBody.status = 0;
+    drawBody.stakeholders = [];
+
+    const createdDoc = await this.drawsRef
+      .doc(drawBody.uuid)
+      .set(drawBody)
+      .catch((err) => {
+        console.error(err);
+        throw new WsException('ERR_CREATE_DRAW');
+      });
 
     if (!createdDoc) {
       throw new WsException('ERR_CREATE_DRAW');
     }
 
-    client.emit('myDrawCreated', data);
+    client.emit('myDrawCreated', drawBody);
   }
 
   @UseGuards(FirebaseAuthGuard)
   @SubscribeMessage('getDraw')
   async onGetDraw(
-    @MessageBody() data: { user: any; drawId: string },
+    @MessageBody() data: { user: any; drawUuid: string },
     @ConnectedSocket() client: Socket,
   ) {
-    const draw = this.draws.find((draw) => draw.uuid === data.drawId);
+    const drawDoc = this.drawsRef.doc(data.drawUuid);
+
+    const drawSnapshot = await drawDoc.get();
+
+    if (!drawSnapshot.exists) {
+      throw new WsException('DRAW_NOT_FOUND');
+    }
+
+    const draw = drawSnapshot.data() as Draw;
 
     if (!draw) {
       throw new WsException('DRAW_NOT_FOUND');
     }
 
-    draw.candidates = await Promise.all(
-      draw.candidates.map(async (user: any) => {
-        const userRecord = await auth().getUser(user.id);
+    draw.stakeholders = (await Promise.all(
+      (draw.stakeholders || []).map(async (user: any) => {
+        const userRecord = await auth().getUser(user.uid || user.id);
 
-        return {
-          uid: userRecord.uid,
-          email: userRecord.email,
-          firstName: userRecord.displayName.split(' ')[0],
-          lastName: userRecord.displayName.split(' ', 2)[1],
-          avatar: userRecord.photoURL,
-        };
+        return new Candidate<any>({
+          id: userRecord.uid,
+          indexes: user.indexes,
+          eligible: true,
+          profile: {
+            email: userRecord.email,
+            firstName: userRecord.displayName.split(' ')[0],
+            lastName: userRecord.displayName.split(' ', 2)[1],
+            avatar: userRecord.photoURL,
+          },
+        });
       }),
-    );
+    )) as Candidate<any>[];
 
     console.log('Draw before emit', draw);
 
@@ -122,28 +145,37 @@ export class DrawGateway
   @UseGuards(FirebaseAuthGuard)
   @SubscribeMessage('joinDraw')
   async onJoinDraw(
-    @MessageBody() data: { user: any; drawId: string },
+    @MessageBody() data: { stakeholder: Stakeholder; drawUuid: string },
     @ConnectedSocket() client: Socket,
   ) {
-    client.join(data.drawId);
+    const drawDoc = this.drawsRef.doc(data.drawUuid);
 
-    const event = {
-      timestamp: new Date().getTime(),
-      type: 'CANDIDATE_SUBSCRIBED',
-      data,
-    };
+    const drawSnapshot = await drawDoc.get();
 
-    const draw = this.draws.find((draw) => draw.uuid === data.drawId);
-
-    if (!draw) {
+    if (!drawSnapshot.exists) {
       throw new WsException('DRAW_NOT_FOUND');
     }
 
-    const drawDoc = this.drawsRef.doc(`${draw.uuid}`);
+    const draw = drawSnapshot.data() as Draw;
+
+    if (
+      draw.stakeholders &&
+      draw.stakeholders.find((stk) => stk.id === data.stakeholder.id)
+    ) {
+      client.emit('drawJoined');
+      throw new WsException('STAKEHOLDER_ALREADY_REGISTERED');
+    }
+
+    console.log(`🚀 ~ file: draw.gateway.ts ~ line 177 ~ data`, data);
+    const candidate: Candidate = {
+      ...data.stakeholder,
+      indexes: [draw.stakeholders ? draw.stakeholders.length : 0],
+      eligible: true,
+    };
 
     const joined = await drawDoc
       .update({
-        candidates: firestore.FieldValue.arrayUnion(data.user),
+        stakeholders: firestore.FieldValue.arrayUnion(candidate),
       })
       .catch((err) => {
         console.error('Error updating doc', err);
@@ -154,6 +186,97 @@ export class DrawGateway
       client.emit('drawJoined');
     }
 
-    client.to(data.drawId).emit('drawEvent', event);
+    const fullCandidate = (await this.getStakeholderFromUserId(
+      data.stakeholder.id,
+    )) as Candidate;
+    fullCandidate.eligible = true;
+    fullCandidate.indexes = candidate.indexes;
+
+    const event: DrawEvent = {
+      timestamp: new Date().getTime(),
+      type: DrawEventType.CANDIDATE_SUBSCRIBED,
+      data: fullCandidate,
+      drawUuid: data.drawUuid,
+    };
+
+    console.log(`🚀 ~ file: draw.gateway.ts ~ line 206 ~ event`, event);
+    console.log('Rooms client is in', client.rooms);
+    client.server.to(data.drawUuid).emit('drawEvent', event);
+  }
+
+  @UseGuards(FirebaseAuthGuard)
+  @SubscribeMessage('listenDraw')
+  async onListenToDraw(
+    @MessageBody() data: { user: any; drawUuid: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.join(data.drawUuid);
+
+    const event: DrawEvent = {
+      timestamp: new Date().getTime(),
+      type: DrawEventType.STAKEHOLDER_SUBSCRIBED,
+      data: await this.getStakeholderFromUserId(data.user.id || data.user.uid),
+      drawUuid: data.drawUuid,
+    };
+
+    const draw = this.draws.find((draw) => draw.uuid === data.drawUuid);
+
+    if (!draw) {
+      throw new WsException('DRAW_NOT_FOUND');
+    }
+
+    client.emit('drawListened', event);
+  }
+
+  @UseGuards(FirebaseAuthGuard)
+  @SubscribeMessage('postToDraw')
+  async onPostToDraw(
+    @MessageBody() event: DrawEvent,
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log('> Event to draw ', event);
+
+    if (event.from && event.from.id) {
+      const stakeholder = await this.getStakeholderFromUserId(event.from.id);
+      if (!stakeholder) {
+        throw new Error('No valid stakeholder found');
+      }
+      event.from = stakeholder;
+    }
+
+    event.eventId = (+new Date()).toString(36);
+
+    client.server.to(event.drawUuid).emit('drawEvent', event);
+
+    if (event.type === DrawEventType.STATUS_CHANGED) {
+      const drawDoc = await this.drawsRef.doc(event.drawUuid).get();
+
+      if (drawDoc.exists && drawDoc.data().status !== event.data) {
+        await this.drawsRef.doc(event.drawUuid).update({
+          status: event.data,
+        });
+      }
+    }
+
+    client.emit('eventPosted', true);
+  }
+
+  async getStakeholderFromUserId(userId: string) {
+    const userRecord = await auth().getUser(userId);
+
+    if (!userRecord) {
+      return undefined;
+    }
+
+    return new Stakeholder<any>({
+      id: userRecord.uid,
+      eligible: true,
+      profile: {
+        email: userRecord.email,
+        firstName: userRecord.displayName.split(' ')[0],
+        lastName: userRecord.displayName.split(' ', 2)[1],
+        avatar: userRecord.photoURL,
+      },
+    });
   }
 }
---
+++ src/draw/entities/draw.entity.ts
@@ -0,0 +1,66 @@
+import { Stakeholder } from './stakeholder.entity';
+import { DrawStatus } from '../enums/draw-status.enum';
+import { DrawData } from '../interfaces/draw-data.interface';
+import { Commit } from '../../commit-reveal/interfaces/commit.interface';
+import { Reveal } from '../../commit-reveal/interfaces/reveal.interface';
+import { Candidate } from './candidate.entity';
+
+export class Draw<D = DrawData> {
+  /**
+   * Unique identifier of a draw
+   */
+  public readonly uuid?: string;
+
+  /**
+   * Additional information related to the draw.
+   * Using the D generic type (default is any);
+   */
+  public readonly data?: D;
+
+  /**
+   * The maximum number of candidates required to automatically start the draw.
+   */
+  public spots?: number;
+
+  /**
+   * The minimum number of candidates required to automatically start the draw.
+   */
+  public minSpots?: number;
+
+  /**
+   * The user id of the creator user
+   */
+  public creatorId?: string;
+
+  /**
+   * Current phase of draw.
+   */
+  public status?: DrawStatus;
+
+  /**
+   * The candidate that was drawn
+   */
+  public winner?: Candidate;
+
+  /**
+   * List of participants that can contribute to the draw.
+   * Not all of them must be elegible to be drawn.
+   */
+  public stakeholders?: Stakeholder[] = [];
+
+  /**
+   * List of all commits registered in the draw
+   */
+  public readonly commits?: Commit[] = [];
+
+  /**
+   * List of all reveals registered in the draw
+   */
+  public readonly reveals?: (Reveal & { valid?: boolean })[] = [];
+
+  constructor(draw?: Draw<D>) {
+    if (draw) {
+      Object.assign(this, draw);
+    }
+  }
+}
